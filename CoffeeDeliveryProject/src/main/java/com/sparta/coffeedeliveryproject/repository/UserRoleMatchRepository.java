6472b38f6286e266e024ce494e103c985e24fdc8
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-20 21:14:31 +0900

    🔥 del - #2 UserRoleMatch 관련 삭제

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/UserRoleMatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.coffeedeliveryproject.repository;
-
-import com.sparta.coffeedeliveryproject.entity.UserRoleMatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleMatchRepository extends JpaRepository<UserRoleMatch, Long> {
-}
