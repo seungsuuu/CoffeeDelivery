0e3897bf5af64ecb62154dd9407d91aac584ac94
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-20 11:14:50 +0900

    ✨ update - #2 예전 비밀번호를 db에 저장하고 user_id와 join하는 table과 colum구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/User.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/User.java
@@ -5,6 +5,9 @@ import com.sparta.coffeedeliveryproject.enums.UserStatusEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Entity
 @Getter
 @Table(name = "users")
@@ -29,6 +32,14 @@ public class User {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserStatusEnum userStatus;
+
+    // 컬렉션 객체임을 알려줌
+    @ElementCollection
+    // pastPasswords의 데이터를 past_passwords 테이블에 저장후에 user_id의 컬럼과 join
+    @CollectionTable(name = "past_passwords", joinColumns = @JoinColumn(name = "user_id"))
+    // pastPasswords 테이블의 컬렉션 값이 past_password로 저장
+    @Column(name = "past_password")
+    private Queue<String> pastPasswords = new LinkedList<>();
 
     @Column(nullable = false)
     private String refreshToken;
