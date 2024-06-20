6d9e6a3a736a9b79493c3c57697ac8bbcc536a94
seungsuuu <tmdtn013@naver.com>
2024-06-20 16:08:49 +0900

    ✨ update - #9 Cafe Entity 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Cafe.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Cafe.java
@@ -1,4 +1,36 @@
 package com.sparta.coffeedeliveryproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "cafe")
 public class Cafe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cafeId;
+
+    @Column(name = "cafe_name", nullable = false)
+    private String cafeName;
+
+    @Column(name = "cafe_info", nullable = false)
+    private String cafeInfo;
+
+    @Column(name = "cafe_address", nullable = false)
+    private String cafeAddress;
+
+    @Column(name = "cafe_like_count")
+    private Long cafeLikeCount;
+
+    public Cafe(String cafeName, String cafeInfo, String cafeAddress) {
+        this.cafeName = cafeName;
+        this.cafeInfo = cafeInfo;
+        this.cafeAddress = cafeAddress;
+        this.cafeLikeCount = 0L;
+    }
+
 }
