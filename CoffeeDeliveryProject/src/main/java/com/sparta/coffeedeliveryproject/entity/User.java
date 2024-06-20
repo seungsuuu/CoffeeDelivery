696b41ec81cc1282cf8b8cc23bdf3fc980ae8faf
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-20 10:52:02 +0900

    🎉 add - #2 User entity 생성 후 구현 (연관관계 구현 x)

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/User.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/User.java
@@ -1,4 +1,35 @@
 package com.sparta.coffeedeliveryproject.entity;
 
+import com.sparta.coffeedeliveryproject.enums.UserRoleEnum;
+import com.sparta.coffeedeliveryproject.enums.UserStatusEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum userRole;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserStatusEnum userStatus;
+
+    @Column(nullable = false)
+    private String refreshToken;
 }
