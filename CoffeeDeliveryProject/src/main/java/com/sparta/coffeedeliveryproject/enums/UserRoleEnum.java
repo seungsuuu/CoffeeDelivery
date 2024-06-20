86a0fabf866fc45e3aca9072beff5daf004bea99
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-20 10:42:09 +0900

    🎉 add - #2 유저 role을 나타내는 UserRoleEnum 생성 후 구현

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/enums/UserRoleEnum.java
@@ -0,0 +1,22 @@
+package com.sparta.coffeedeliveryproject.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRoleEnum {
+
+    USER(Authority.USER), // 일반 유저
+    ADMIN(Authority.ADMIN); // 관리자
+
+    private final String role;
+
+    UserRoleEnum(String role) {
+        this.role = role;
+    }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+    
+}
