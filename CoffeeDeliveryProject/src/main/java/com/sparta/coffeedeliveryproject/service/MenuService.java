4e94323ced0492bba0c3d6cc9f2b83d444f4223f
seungsuuu <tmdtn013@naver.com>
2024-06-20 19:54:48 +0900

    ✨ update - #13 MenuService에 deleteMenu 메서드 로직 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/MenuService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/MenuService.java
@@ -2,6 +2,7 @@ package com.sparta.coffeedeliveryproject.service;
 
 import com.sparta.coffeedeliveryproject.dto.MenuRequestDto;
 import com.sparta.coffeedeliveryproject.dto.MenuResponseDto;
+import com.sparta.coffeedeliveryproject.dto.MessageResponseDto;
 import com.sparta.coffeedeliveryproject.entity.Menu;
 import com.sparta.coffeedeliveryproject.repository.MenuRepository;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,16 @@ public class MenuService {
         Menu saveMenu = menuRepository.save(menu);
 
         return new MenuResponseDto(saveMenu);
+    }
+
+    public MessageResponseDto deleteMenu(Long menuId) {
+
+        Menu menu = menuRepository.findById(menuId).orElseThrow(
+                () -> new IllegalArgumentException("해당 메뉴를 찾을 수 없습니다.")
+        );
+
+        menuRepository.delete(menu);
+
+        return new MessageResponseDto("삭제 완료 되었습니다.");
     }
 }
