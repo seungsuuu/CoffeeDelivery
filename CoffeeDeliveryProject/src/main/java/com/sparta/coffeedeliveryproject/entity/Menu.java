629e73d163dd29d29b31329c6c97e3595f3d20e4
seungsuuu <tmdtn013@naver.com>
2024-06-20 10:47:27 +0900

    ✨ update - #1 Menu Entity 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Menu.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Menu.java
@@ -1,4 +1,36 @@
 package com.sparta.coffeedeliveryproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "menu")
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long menuId;
+
+    @Column(name = "menu_name", nullable = false)
+    private String menuName;
+
+    @Column(name = "menu_price", nullable = false)
+    private Long menuPrice;
+
+//    @JoinColumn(name = "cafe_id", nullable = false)
+//    private Cafe cafe;
+
+    @Column(name = "cafe_id", nullable = false)
+    private Long cafeId;
+
+    public Menu(String menuName, Long menuPrice, Long cafeId) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.cafeId = cafeId;
+//        this.cafe = cafe;
+    }
+
 }
