148a8176d8a56800a528e1c7498df7036bbf8f1e
seungsuuu <tmdtn013@naver.com>
2024-06-20 16:17:28 +0900

    🎉 add - #9 Cafe Controller, Service, Repository 초기 세팅

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/CafeService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/CafeService.java
@@ -1,4 +1,16 @@
 package com.sparta.coffeedeliveryproject.service;
 
+import com.sparta.coffeedeliveryproject.repository.CafeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CafeService {
+
+    private final CafeRepository cafeRepository;
+
+    public CafeService(CafeRepository cafeRepository) {
+        this.cafeRepository = cafeRepository;
+    }
+
+
 }
