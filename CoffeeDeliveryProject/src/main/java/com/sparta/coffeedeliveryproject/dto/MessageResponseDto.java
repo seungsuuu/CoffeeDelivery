28a0294d7ce83065208612f0300faa0846074bd8
seungsuuu <tmdtn013@naver.com>
2024-06-20 19:28:36 +0900

    🩹 fix - #13 MessageResponseDto 생성자 수정

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/MessageResponseDto.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/MessageResponseDto.java
@@ -1,11 +1,14 @@
 package com.sparta.coffeedeliveryproject.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
 @Getter
-@RequiredArgsConstructor
 public class MessageResponseDto {
 
     private String message;
+
+    public MessageResponseDto(String message) {
+        this.message = message;
+    }
 
 }
