91e3a6598240539eed1a78eccf4428969ad18293
seungsuuu <tmdtn013@naver.com>
2024-06-20 13:10:34 +0900

    🩹 fix - #1 RequestMapping 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/MenuController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/MenuController.java
@@ -5,12 +5,10 @@ import com.sparta.coffeedeliveryproject.dto.MenuResponseDto;
 import com.sparta.coffeedeliveryproject.service.MenuService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/admin/cafes")
+@RestController
+@RequestMapping("/admin/cafes")
 public class MenuController {
 
     private final MenuService menuService;
