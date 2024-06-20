1e053488812914b66e4207fdfed88715f1c2f92d
pkhyun <pkh954533@gmail.com>
2024-06-20 18:13:29 +0900

    🩹 fix - #4 Order entity Table 이름 orders로 수정(sql예약어)

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Order.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Order.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Entity
 @Getter
-@Table
+@Table(name = "orders")
 @NoArgsConstructor
 public class Order extends TimeStamped{
 
@@ -23,26 +23,24 @@ public class Order extends TimeStamped{
     @Enumerated(value = EnumType.STRING)
     private OrderStatus orderStatus;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
 
     @ManyToOne
     @JoinColumn(name = "cafe_id")
     private Cafe cafe;
 
-    // 컬렉션 객체임을 알려줌
-    @ElementCollection
-    // menuNames의 데이터를 menu_names 테이블에 저장 후에 order_id의 컬럼과 join
-    @CollectionTable(name = "menu_names", joinColumns = @JoinColumn(name = "order_id"))
-    // menuNames 테이블의 컬렉션 값이 menu_name로 저장
-    @Column(name = "menu_name")
-    private List<String> menuNames; // 메뉴 이름 리스트
 
-    public Order(int orderPrice, OrderStatus orderStatus, User user, Cafe cafe, List<String> menuNames) {
+    @ElementCollection
+    @CollectionTable(name = "menu_names", joinColumns = @JoinColumn(name = "order_id"))
+    @Column(name = "menu_name")
+    private List<String> menuNames;
+
+    public Order(int orderPrice, OrderStatus orderStatus/*, User user*/, Cafe cafe, List<String> menuNames) {
         this.orderPrice = orderPrice;
         this.orderStatus = orderStatus;
-        this.user = user;
+//        this.user = user;
         this.cafe = cafe;
         this.menuNames = menuNames;
     }
