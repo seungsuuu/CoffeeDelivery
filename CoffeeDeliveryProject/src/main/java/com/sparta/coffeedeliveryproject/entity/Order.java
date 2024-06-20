8ca33ed40f792f60fcd21df32434b0695f467373
pkhyun <pkh954533@gmail.com>
2024-06-20 11:37:00 +0900

    🎉 add - #4 Order Entity 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Order.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/entity/Order.java
@@ -1,4 +1,38 @@
 package com.sparta.coffeedeliveryproject.entity;
 
-public class Order {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table
+@NoArgsConstructor
+public class Order extends TimeStamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private int orderPrice;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "cafe_id")
+    private Cafe cafe;
+
+    public Order(int orderPrice, OrderStatus orderStatus, User user, Cafe cafe) {
+        this.orderPrice = orderPrice;
+        this.orderStatus = orderStatus;
+        this.user = user;
+        this.cafe = cafe;
+    }
 }
