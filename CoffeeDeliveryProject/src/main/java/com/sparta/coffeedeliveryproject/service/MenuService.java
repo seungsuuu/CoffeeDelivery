f7d19ffa6b3db11247fca616690a65f8fba8794a
seungsuuu <tmdtn013@naver.com>
2024-06-20 13:08:16 +0900

    ✨ update - #1 MenuService에 createMenu 메서드 로직 구현 및 MenuRepository에 JpaRepository 상속 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/MenuService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/MenuService.java
@@ -1,4 +1,28 @@
 package com.sparta.coffeedeliveryproject.service;
 
+import com.sparta.coffeedeliveryproject.dto.MenuRequestDto;
+import com.sparta.coffeedeliveryproject.dto.MenuResponseDto;
+import com.sparta.coffeedeliveryproject.entity.Menu;
+import com.sparta.coffeedeliveryproject.repository.MenuRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    public MenuService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    public MenuResponseDto createMenu(Long cafeId, MenuRequestDto requestDto) {
+
+        String menuName = requestDto.getMenuName();
+        Long menuPrice = requestDto.getMenuPrice();
+        Menu menu = new Menu(menuName, menuPrice, cafeId);
+
+        Menu saveMenu = menuRepository.save(menu);
+
+        return new MenuResponseDto(saveMenu);
+    }
 }
