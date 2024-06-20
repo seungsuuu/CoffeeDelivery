2d1ec4dd4042e61db399842941a1b40136ef30d7
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-20 22:35:50 +0900

    ✨ feat - #19 유저 초기 데이터 세팅 구현, 유저 role 추가 되는 것 테스트

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/initalizer/DataInitializer.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/initalizer/DataInitializer.java
@@ -2,37 +2,30 @@ package com.sparta.coffeedeliveryproject.initalizer;
 
 import com.sparta.coffeedeliveryproject.entity.User;
 import com.sparta.coffeedeliveryproject.entity.UserRole;
-import com.sparta.coffeedeliveryproject.entity.UserRoleMatch;
 import com.sparta.coffeedeliveryproject.enums.UserStatusEnum;
 import com.sparta.coffeedeliveryproject.repository.UserRepository;
-import com.sparta.coffeedeliveryproject.repository.UserRoleMatchRepository;
 import com.sparta.coffeedeliveryproject.repository.UserRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRoleRepository userRoleRepository;
-
     private final UserRepository userRepository;
-
-    private final UserRoleMatchRepository userRoleMatchRepository; // UserRoleMatch의 Repository 추가
 
     @Override
     public void run(String... args) throws Exception {
-
         if (userRoleRepository.count() == 0) {
 
             // 유저 역할 초기 데이터
-            UserRole userRole = new UserRole();
-            userRole.setRole("USER");
-
-            // 관리자 역할 초기 데이터
-            UserRole adminRole = new UserRole();
-            adminRole.setRole("ADMIN");
+            UserRole userRole = new UserRole("USER");
+            UserRole adminRole = new UserRole("ADMIN");
 
             userRoleRepository.save(userRole);
             userRoleRepository.save(adminRole);
@@ -47,43 +40,26 @@ public class DataInitializer implements CommandLineRunner {
             UserRole adminRole = userRoleRepository.findByRole("ADMIN")
                     .orElseThrow(() -> new RuntimeException("Role not found: ADMIN"));
 
-            User user = new User("user1", "password123", "NickName1", userRole, UserStatusEnum.ACTIVE, "refresh_token_1");
+            Set<UserRole> user1Roles = new HashSet<>();
+            user1Roles.add(userRole);
 
-            User user2 = new User("user2", "password12223", "NickName2", userRole, UserStatusEnum.ACTIVE, "refresh_token_1");
+            Set<UserRole> user2Roles = new HashSet<>();
+            user2Roles.add(userRole);
 
-            User user3 = new User("user3", "password123", "NickName3", adminRole, UserStatusEnum.ACTIVE, "refresh_token_1");
+            Set<UserRole> user3Roles = new HashSet<>();
+            user3Roles.add(userRole); // 관리자는 USER 역할과 ADMIN 역할을 모두 가짐
+            user3Roles.add(adminRole);
 
-            userRepository.save(user);
+            User user1 = new User("user1", "password123", "NickName1", user1Roles, UserStatusEnum.ACTIVE, "refresh_token_1");
+            User user2 = new User("user2", "password12223", "NickName2", user2Roles, UserStatusEnum.ACTIVE, "refresh_token_1");
+            User user3 = new User("user3", "password123", "NickName3", user3Roles, UserStatusEnum.ACTIVE, "refresh_token_1");
 
+            // 유저 role 추가되는 것이 가능한지 테스트
+            user2.addUserRoles(adminRole);
+
+            userRepository.save(user1);
             userRepository.save(user2);
-
             userRepository.save(user3);
-
-            // UserRoleMatch 엔티티 초기화 및 저장
-            UserRoleMatch match1 = new UserRoleMatch();
-
-            match1.setUser(user);
-
-            match1.setUserRole(userRole);
-
-            UserRoleMatch match2 = new UserRoleMatch();
-
-            match2.setUser(user2);
-
-            match2.setUserRole(userRole);
-
-            UserRoleMatch match3 = new UserRoleMatch();
-
-            match3.setUser(user3);
-
-            match3.setUserRole(adminRole);
-
-            userRoleMatchRepository.save(match1);
-
-            userRoleMatchRepository.save(match2);
-
-            userRoleMatchRepository.save(match3);
-
         }
     }
 }
