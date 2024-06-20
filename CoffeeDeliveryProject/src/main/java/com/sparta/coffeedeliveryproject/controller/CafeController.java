3c0ac910aa52e8846540555cf4c96aa24b83842a
seungsuuu <tmdtn013@naver.com>
2024-06-20 17:13:16 +0900

    🩹 fix - #9 공백 제거

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/CafeController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/CafeController.java
@@ -14,5 +14,4 @@ public class CafeController {
         this.cafeService = cafeService;
     }
 
-
 }
--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/CafeService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/CafeService.java
@@ -12,5 +12,4 @@ public class CafeService {
         this.cafeRepository = cafeRepository;
     }
 
-
 }
