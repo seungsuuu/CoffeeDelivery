fe68520d54b262a31f712a68ba4d640ddc45192e
hyunmang2 <wowow19811@gmail.com>
2024-06-21 19:47:47 +0900

    🎉 add - #6 Security 구현(config, Filter 미구현)

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/security/UserDetailsImpl.java
@@ -0,0 +1,67 @@
+package com.sparta.coffeedeliveryproject.security;
+
+import com.sparta.coffeedeliveryproject.entity.User;
+import com.sparta.coffeedeliveryproject.enums.UserStatusEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 사용자의 상태를 가져온다.
+        UserStatusEnum role = user.getUserStatus();
+        // 가져온 사용자의 상태를 문자열로 바꿔준다.
+        String authority = role.getStatus();
+
+        // simpleGrantedAuthority 객체를 만들어준다.
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        // authorities 컬렉션에 권한이 들어있는 객체를 넣어준다.
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
