243d38e23180aeca0227719c68069651e67d22c4
seungsuuu <tmdtn013@naver.com>
2024-06-21 10:30:45 +0900

    ✨ update - #16 CafeController createCafe API 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/CafeController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/CafeController.java
@@ -1,17 +1,29 @@
 package com.sparta.coffeedeliveryproject.controller;
 
+import com.sparta.coffeedeliveryproject.dto.CafeRequestDto;
+import com.sparta.coffeedeliveryproject.dto.CafeResponseDto;
 import com.sparta.coffeedeliveryproject.service.CafeService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/cafes")
 public class CafeController {
 
     private final CafeService cafeService;
 
     public CafeController(CafeService cafeService) {
         this.cafeService = cafeService;
+    }
+
+    @PostMapping("/admin/cafes")
+    public ResponseEntity<CafeResponseDto> createCafe(@RequestBody CafeRequestDto requestDto) {
+
+        CafeResponseDto responseDto = cafeService.createCafe(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
 }
