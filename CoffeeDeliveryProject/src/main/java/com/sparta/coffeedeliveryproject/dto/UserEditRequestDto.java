41b6f14576c42acd45da0f3021acaa9282e9cc37
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 12:43:47 +0900

    🩹 fix - #24 UserEditRequestDto에 Getter, password 필드 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/UserEditRequestDto.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/UserEditRequestDto.java
@@ -1,10 +1,15 @@
 package com.sparta.coffeedeliveryproject.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UserEditRequestDto {
 
     private String newUserName;
 
     private String newNickName;
+
+    private String password;
 
     private String newPassword;
 
