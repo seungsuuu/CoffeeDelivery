6d4cd42e456b6a343ba3c6c998d10e72cfedeb30
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 12:41:50 +0900

    🎉 add - #23 UserEditRequestDto 생성

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/UserEditRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.coffeedeliveryproject.dto;
+
+public class UserEditRequestDto {
+
+    private String newUserName;
+
+    private String newNickName;
+
+    private String newPassword;
+
+}
