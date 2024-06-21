802dbd85ad054109df63e98a8874190f25a3aaf0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 15:10:14 +0900

    🎉 add - #24 커스텀 에러 생성

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/exceptions/RecentlyUsedPasswordException.java
@@ -0,0 +1,12 @@
+package com.sparta.coffeedeliveryproject.exceptions;
+
+public class RecentlyUsedPasswordException extends RuntimeException{
+
+    public RecentlyUsedPasswordException(String message) {
+
+        super(message);
+
+    }
+
+}
+
