0d8fb2a82583475fd1773f704c8ec33f51973052
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 11:26:32 +0900

    ✨ update, 🎉add - #2 AdminUserService생성 후 getAllUsers 메서드 구현

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/AdminUserService.java
@@ -0,0 +1,24 @@
+package com.sparta.coffeedeliveryproject.service;
+
+import com.sparta.coffeedeliveryproject.dto.UserResponseDto;
+import com.sparta.coffeedeliveryproject.entity.User;
+import com.sparta.coffeedeliveryproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+@RequiredArgsConstructor
+public class AdminUserService {
+    private final UserRepository userRepository;
+
+    public List<UserResponseDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        // 모든 유저를 UserResponseDto로 매핑하여 list로 들어 반환
+        return users.stream()
+                .map(user -> new UserResponseDto(user))
+                .collect(Collectors.toList());
+    }
+}
