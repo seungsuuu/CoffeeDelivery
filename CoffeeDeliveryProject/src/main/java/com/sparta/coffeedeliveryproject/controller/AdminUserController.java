adea65007f4c61546ca84685c7151eb06a9fc0c3
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 11:35:00 +0900

    ✨ update - #2 AdminUserController에 전체 유저 조회 url 생성

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
@@ -1,4 +1,23 @@
 package com.sparta.coffeedeliveryproject.controller;
 
+import com.sparta.coffeedeliveryproject.dto.UserResponseDto;
+import com.sparta.coffeedeliveryproject.service.AdminUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminUserController {
+    private final AdminUserService adminUserService;
+
+    // 전체 유저 조회
+    @GetMapping("/users")
+    public List<UserResponseDto> gellAllUsers() {
+        return adminUserService.getAllUsers();
+    }
 }
