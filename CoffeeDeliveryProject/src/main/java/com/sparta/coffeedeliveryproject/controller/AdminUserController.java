226deae248afab31fdba60614a3a21bdba193583
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-21 15:09:41 +0900

    ✨ update - #23 AdminUserController에 editUser url 생성

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
@@ -1,11 +1,15 @@
 package com.sparta.coffeedeliveryproject.controller;
 
+import com.sparta.coffeedeliveryproject.dto.UserEditRequestDto;
 import com.sparta.coffeedeliveryproject.dto.UserResponseDto;
+import com.sparta.coffeedeliveryproject.exceptions.PasswordMismatchException;
+import com.sparta.coffeedeliveryproject.exceptions.RecentlyUsedPasswordException;
+import com.sparta.coffeedeliveryproject.exceptions.UserNotFoundException;
 import com.sparta.coffeedeliveryproject.service.AdminUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +24,27 @@ public class AdminUserController {
     @GetMapping("/users")
     public List<UserResponseDto> gellAllUsers() {
         return adminUserService.getAllUsers();
+    }
+
+    // 특정 유저 수정
+    @PutMapping("/users/{userId}")
+    public UserResponseDto editUser(@PathVariable Long userId, @RequestBody UserEditRequestDto userEditRequestDto) {
+        return adminUserService.editUser(userId, userEditRequestDto);
+    }
+
+    @ExceptionHandler // 에러 핸들링
+    private ResponseEntity<String> handleException(RecentlyUsedPasswordException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler // 에러 핸들링
+    private ResponseEntity<String> handleException(UserNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler // 에러 핸들링
+    private ResponseEntity<String> handleException(PasswordMismatchException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
