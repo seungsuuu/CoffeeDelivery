f685499c4c183272a200cadd683ef846842b27e2
dlalwn <annie45671@gmail.com>
2024-06-22 17:26:40 +0900

    ✨ update - #1 review Repository 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/ReviewRepository.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/ReviewRepository.java
@@ -1,4 +1,9 @@
 package com.sparta.coffeedeliveryproject.repository;
 
-public interface ReviewRepository {
+import com.sparta.coffeedeliveryproject.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long>{
 }
