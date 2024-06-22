883ed25a23dbfa73636e2f33aecf96c11d4e2936
dlalwn <annie45671@gmail.com>
2024-06-22 22:37:04 +0900

    ✨ update - #54 review Controller, Service 리뷰 수정 기능 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/ReviewController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/ReviewController.java
@@ -2,14 +2,12 @@ package com.sparta.coffeedeliveryproject.controller;
 
 import com.sparta.coffeedeliveryproject.dto.ReviewRequestDto;
 import com.sparta.coffeedeliveryproject.dto.ReviewResponseDto;
-import com.sparta.coffeedeliveryproject.entity.Review;
 import com.sparta.coffeedeliveryproject.service.ReviewService;
 import jakarta.validation.Valid;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cafes")
@@ -21,9 +19,29 @@ public class ReviewController {
     @PostMapping("/{cafeId}/orders/{orderId}/reviews")
     public ReviewResponseDto createReview(@PathVariable Long cafeId,
                                           @PathVariable Long orderId,
-                                          @Valid @RequestBody ReviewRequestDto requestDto
-            /*@AuthenticationPrincipal UserDetailsImpl userDetails*/){
+                                          @Valid @RequestBody ReviewRequestDto requestDto/*,
+                                          @AuthenticationPrincipal UserDetailsImpl userDetails*/){
         return reviewService.createReview(cafeId, orderId, requestDto/*, userDetails.getUser()*/);
+    }
+
+//    @GetMapping("/{cafeId}/reviews")
+//    public ReviewResponseDto getReview(@PathVariable Long cafeId,
+//                                        @RequestBody ReviewRequestDto requestDto){
+//
+//        return reviewService.getReview(cafeId);
+//
+//    }
+
+//    @GetMapping("/reviews")
+//    public List<ReviewResponseDto> getReview(){
+//        return reviewService.getReview();
+//    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ReviewResponseDto updateReview(@PathVariable Long reviewId,
+                                          @Valid @RequestBody ReviewRequestDto requestDto/*,
+                                          @AuthenticationPrincipal UserDetailsImpl userDetails*/){
+        return reviewService.updateReview(reviewId, requestDto/*, userDetails.getUser()*/);
     }
 
 }
--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/ReviewService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/ReviewService.java
@@ -10,6 +10,9 @@ import com.sparta.coffeedeliveryproject.repository.OrderRepository;
 import com.sparta.coffeedeliveryproject.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,21 +23,63 @@ public class ReviewService {
     private final OrderRepository orderRepository;
 
     public ReviewResponseDto createReview(Long cafeId, Long orderId,ReviewRequestDto requestDto/*,User user*/){
+
         Cafe cafe = findCafeById(cafeId);
         Order order = findOrderById(orderId);
         Review review = new Review(requestDto, cafe, order);
         Review saveReview = reviewRepository.save(review);
         return new ReviewResponseDto(review);
+
+    }
+
+//    public ReviewResponseDto getReview(Long cafeId, ReviewRequestDto requestDto) {
+//
+//        Cafe cafe = findCafeById(cafeId);
+//        Review review = new Review(requestDto, cafe);
+//        Review getReview = reviewRepository.findAll(review);
+//
+//        return new ReviewResponseDto(review);
+//
+//    }
+
+//    public List<ReviewResponseDto> getReview() {
+//
+//        return reviewRepository.findAllByReviewByCreatedAtDesc().stream().map(ReviewResponseDto::new).toList();
+//    }
+
+    @Transactional
+    public ReviewResponseDto updateReview(Long reviewId, ReviewRequestDto requestDto/*, User user*/) {
+
+        Review review = findReviewById(reviewId);
+        review.update(requestDto);
+        return new ReviewResponseDto(review);
+        /*
+        if(review.getUser().getId() == user.getId()){
+            review.update(requestDto);
+            return new ReviewResponseDto(review);
+         }else throw new IllegalArgumentException("본인이 작성한 리뷰만 수정 가능합니다.");
+        */
+    }
+
+    private Review findReviewById(Long reviewId) {
+
+        return reviewRepository.findById(reviewId).orElseThrow(() ->
+                new IllegalArgumentException("해당 리뷰를 찾을 수 없습니다."));
+
     }
 
     private Order findOrderById(Long orderId) {
+
         return orderRepository.findById(orderId).orElseThrow(() ->
                 new IllegalArgumentException("해당 주문을 찾을 수 없습니다."));
+
     }
 
     private Cafe findCafeById(Long cafeId) {
+
         return cafeRepository.findById(cafeId).orElseThrow(() ->
                 new IllegalArgumentException("해당 카페를 찾을 수 없습니다."));
+
     }
 
 }
