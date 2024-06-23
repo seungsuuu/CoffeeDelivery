b16fbe6155f09847c7419fda1a278de5f646ecb1
seungsuuu <tmdtn013@naver.com>
2024-06-23 20:43:24 +0900

    ✨ update - #59 MenuRequestDto validation 적용

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/MenuRequestDto.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/MenuRequestDto.java
@@ -1,12 +1,16 @@
 package com.sparta.coffeedeliveryproject.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class MenuRequestDto {
 
+    @NotBlank(message = "[menuName:blank] 메뉴 이름을 입력해주세요!")
     private String menuName;
 
+    @NotNull(message = "[menuPrice:null] 메뉴 가격을 입력해주세요!")
     private Long menuPrice;
 
 }
