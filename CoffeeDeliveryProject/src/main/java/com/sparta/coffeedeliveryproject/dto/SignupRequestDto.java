dd6d9522bc348a19e772c5e155af37cc13063921
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-24 17:02:40 +0900

    🩹 fix - 회원가입시 비밀번호 글자 수 수정

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/SignupRequestDto.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/dto/SignupRequestDto.java
@@ -18,7 +18,7 @@ public class SignupRequestDto {
     private String userName;
 
     @Size(min = 8, max = 15, message = "비밀번호는 최소 8글자 이상, 15글자 이하이어야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])\\S{10,}$", message = "password는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로만 구성되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])\\S{8,}$", message = "password는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로만 구성되어야 합니다.")
     private String password;
 
     @NotBlank
