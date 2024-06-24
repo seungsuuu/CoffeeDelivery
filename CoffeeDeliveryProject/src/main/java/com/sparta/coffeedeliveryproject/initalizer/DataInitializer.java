785bb6fd84a5bf25c6b3b1b0823f6a21213e7180
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-24 19:05:41 +0900

    🩹 fix - 사용하지 않는 레포지토리 삭제

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/initalizer/DataInitializer.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/initalizer/DataInitializer.java
@@ -1,23 +1,16 @@
 package com.sparta.coffeedeliveryproject.initalizer;
 
-import com.sparta.coffeedeliveryproject.entity.User;
 import com.sparta.coffeedeliveryproject.entity.UserRole;
-import com.sparta.coffeedeliveryproject.enums.UserStatusEnum;
-import com.sparta.coffeedeliveryproject.repository.UserRepository;
 import com.sparta.coffeedeliveryproject.repository.UserRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRoleRepository userRoleRepository;
-    private final UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
