f04f8099dfa536aba307c6f8174d4feaacc47d2d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-24 16:29:41 +0900

    ✨ update - #69 관리자 권한 부분 권한 인증 후 사용가능하게 수정

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/AdminUserController.java
@@ -3,13 +3,10 @@ package com.sparta.coffeedeliveryproject.controller;
 import com.sparta.coffeedeliveryproject.dto.MessageResponseDto;
 import com.sparta.coffeedeliveryproject.dto.UserEditRequestDto;
 import com.sparta.coffeedeliveryproject.dto.UserResponseDto;
-import com.sparta.coffeedeliveryproject.exceptions.PasswordMismatchException;
-import com.sparta.coffeedeliveryproject.exceptions.RecentlyUsedPasswordException;
-import com.sparta.coffeedeliveryproject.exceptions.UserNotFoundException;
+import com.sparta.coffeedeliveryproject.security.UserDetailsImpl;
 import com.sparta.coffeedeliveryproject.service.AdminUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,32 +20,32 @@ public class AdminUserController {
 
     // 전체 유저 조회
     @GetMapping("/users")
-    public List<UserResponseDto> gellAllUsers() {
-        return adminUserService.getAllUsers();
+    public List<UserResponseDto> gellAllUsers(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return adminUserService.getAllUsers(userDetails.getUser().getUserId());
     }
 
     // 특정 유저 수정
     @PutMapping("/users/{userId}")
-    public UserResponseDto editUser(@PathVariable Long userId, @RequestBody UserEditRequestDto userEditRequestDto) {
-        return adminUserService.editUser(userId, userEditRequestDto);
+    public UserResponseDto editUser(@PathVariable Long userId, @RequestBody UserEditRequestDto userEditRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return adminUserService.editUser(userId, userEditRequestDto, userDetails.getUser().getUserId());
     }
 
     // 특정 유저 삭제
     @DeleteMapping("/users/{userId}")
-    public MessageResponseDto deleteUser(@PathVariable Long userId) {
-        return adminUserService.deleteUser(userId);
+    public MessageResponseDto deleteUser(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return adminUserService.deleteUser(userId, userDetails.getUser().getUserId());
     }
 
     // 특정 유저 admin으로 권한 부여
     @PutMapping("/users/authority/{userId}")
-    public UserResponseDto changeUserRoleToAdmin(@PathVariable long userId) {
-        return adminUserService.changeUserRoleToAdmin(userId);
+    public UserResponseDto changeUserRoleToAdmin(@PathVariable long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return adminUserService.changeUserRoleToAdmin(userId, userDetails.getUser().getUserId());
     }
 
     // 특정 유저 block
     @PutMapping("/users/block/{userId}")
-    public UserResponseDto userBlock(@PathVariable long userId) {
-        return adminUserService.userBlock(userId);
+    public UserResponseDto userBlock(@PathVariable long userId, UserDetailsImpl userDetails) {
+        return adminUserService.userBlock(userId, userDetails.getUser().getUserId());
     }
 
 }
--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/AdminUserService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/AdminUserService.java
@@ -26,19 +26,26 @@ public class AdminUserService {
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
 
-    public List<UserResponseDto> getAllUsers() {
+    public List<UserResponseDto> getAllUsers(Long adminUserId) {
+        User user = findUserById(adminUserId);
+
+        checkRole(user);
+
         List<User> users = userRepository.findAll();
 
         // 모든 유저를 UserResponseDto로 매핑하여 list로 들어 반환
         return users.stream()
-                .map(user -> new UserResponseDto(user))
+                .map(u -> new UserResponseDto(u))
                 .collect(Collectors.toList());
     }
 
     @Transactional
-    public UserResponseDto editUser(Long userId, UserEditRequestDto userEditRequestDto) {
+    public UserResponseDto editUser(Long userId, UserEditRequestDto userEditRequestDto, Long adminUserId) {
 
         User user = findUserById(userId);
+        User adminUser = findUserById(adminUserId);
+
+        checkRole(adminUser);
 
         if (userEditRequestDto.getNewUserName() != null) {
             user.editUserName(user.getUserName());
@@ -84,8 +91,11 @@ public class AdminUserService {
     }
 
     @Transactional
-    public MessageResponseDto deleteUser(Long userId) {
+    public MessageResponseDto deleteUser(Long userId, Long adminUserId) {
         User user = findUserById(userId);
+        User adminUser = findUserById(adminUserId);
+
+        checkRole(adminUser);
 
         userRepository.delete(user);
 
@@ -93,8 +103,11 @@ public class AdminUserService {
     }
 
     @Transactional
-    public UserResponseDto changeUserRoleToAdmin(Long userId) {
+    public UserResponseDto changeUserRoleToAdmin(Long userId, Long adminUserId) {
         User user = findUserById(userId);
+        User adminUser = findUserById(adminUserId);
+
+        checkRole(adminUser);
 
         UserRole adminRole = userRoleRepository.findByRole("ADMIN")
                 .orElseThrow(() -> new IllegalArgumentException("ADMIN 이라는 권한을 찾지 못하였습니다."));
@@ -110,8 +123,11 @@ public class AdminUserService {
     }
 
     @Transactional
-    public UserResponseDto userBlock(Long userId) {
+    public UserResponseDto userBlock(Long userId, Long adminUserId) {
         User user = findUserById(userId);
+        User adminUser = findUserById(adminUserId);
+
+        checkRole(adminUser);
 
         if(user.getUserStatus() == UserStatusEnum.BLOCK) {
             throw  new IllegalArgumentException("이미 차단된 회원입니다.");
@@ -128,4 +144,11 @@ public class AdminUserService {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("현재 userId로 유저를 찾을 수 없습니다."));
     }
+
+    private void checkRole(User user) {
+        if(!user.getUserRoles().contains(userRoleRepository.findByRole("ADMIN"))) {
+            throw new IllegalArgumentException("관리자만 사용할 수 있는 기능입니다.");
+        }
+    }
+
 }
