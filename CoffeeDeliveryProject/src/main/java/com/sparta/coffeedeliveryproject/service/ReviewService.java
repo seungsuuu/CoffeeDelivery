9ced53b096f4fae3a99fcb1bd8abcf8d2937b8d7
dlalwn <annie45671@gmail.com>
2024-06-24 18:29:53 +0900

    ✨ update - #72 리뷰 관련 본인 인증 적용

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/ReviewService.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/service/ReviewService.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
