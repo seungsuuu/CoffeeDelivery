f4daa351777b560aa68b07016acaa683bd8d1f5d
pkhyun <pkh954533@gmail.com>
2024-06-24 19:05:04 +0900

    ✨ update - #63 리뷰 좋아요 취소기능 구현

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/ReviewLikeController.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/controller/ReviewLikeController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cafes")
@@ -26,4 +23,13 @@ public class ReviewLikeController {
         MessageResponseDto responseDto = reviewLikeService.likeReview(reviewId, userDetails.getUser());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
+
+    //리뷰 좋아요 취소
+    @DeleteMapping("/reviews/{reviewId}/likes")
+    public ResponseEntity<MessageResponseDto> unLikeReview(@PathVariable Long reviewId,
+                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        MessageResponseDto responseDto = reviewLikeService.unlikeReview(reviewId, userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
 }
