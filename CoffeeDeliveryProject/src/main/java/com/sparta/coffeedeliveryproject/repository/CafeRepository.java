2f6e5cd87bb19f753d5ca59f2851bf7f735c5422
seungsuuu <tmdtn013@naver.com>
2024-07-02 17:18:49 +0900

    ✨ update - CafeRepositoryQuery 인터페이스 상속 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepository.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepository.java
@@ -2,9 +2,11 @@ package com.sparta.coffeedeliveryproject.repository;
 
 import com.sparta.coffeedeliveryproject.entity.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface CafeRepository extends JpaRepository<Cafe, Long> {
+@Repository
+public interface CafeRepository extends JpaRepository<Cafe, Long>, CafeRepositoryQuery {
     Optional<Cafe> findByCafeName(String cafeName);
 }
