88fb5e7d915df7895904c7bf0199c1e4c0392a3f
seungsuuu <tmdtn013@naver.com>
2024-07-02 17:20:02 +0900

    ✨ update - Query 메서드를 구현체 클래스에 QueryDsl로 구현

---
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepositoryQueryImpl.java
@@ -0,0 +1,61 @@
+package com.sparta.coffeedeliveryproject.repository;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.coffeedeliveryproject.entity.Cafe;
+import com.sparta.coffeedeliveryproject.entity.QCafe;
+import com.sparta.coffeedeliveryproject.entity.QCafeLike;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CafeRepositoryQueryImpl implements CafeRepositoryQuery {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<Cafe> findByUserLikes(Pageable pageable, Long userId) {
+
+        QCafe cafe = QCafe.cafe;
+        QCafeLike cafeLike = QCafeLike.cafeLike;
+
+        JPAQuery<Cafe> query = jpaQueryFactory.selectFrom(cafe)
+                .leftJoin(cafe.cafeLikeList, cafeLike)
+                .where(cafeLike.user.userId.eq(userId));
+
+        long count = countQuery(userId).fetch().get(0);
+        System.out.println(count);
+
+        query.offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        for (Sort.Order order : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(cafe.getType(), cafe.getMetadata());
+            OrderSpecifier orderSpecifier = new OrderSpecifier(order.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(order.getProperty()));
+            query.orderBy(orderSpecifier);
+        }
+
+        List<Cafe> cafeList = query.fetch();
+
+        return PageableExecutionUtils.getPage(cafeList, pageable, () -> count);
+    }
+
+    private JPAQuery<Long> countQuery(Long userId) {
+        QCafe qCafe = QCafe.cafe;
+        return jpaQueryFactory.select(Wildcard.count)
+                .from(qCafe)
+                .where(qCafe.cafeLikeList.any().user.userId.eq(userId));
+    }
+
+}
