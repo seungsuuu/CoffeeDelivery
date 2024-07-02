c5f96dbcdd1a93b56db2b9f197236ca6d05494e2
seungsuuu <tmdtn013@naver.com>
2024-07-02 17:20:17 +0900

    ♻️ refactor - 옵티마이저

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepositoryQuery.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepositoryQuery.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CafeRepositoryQuery {
     Page<Cafe> findByUserLikes(Pageable pageable, Long userId);
